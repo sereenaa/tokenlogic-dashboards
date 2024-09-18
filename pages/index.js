b2bc87c4ddbf0e3ad9aa3ff73e7f8f8ed3cd4c32
sereenaa <serenax8@gmail.com>
2024-09-18 21:13:12 +1000

    remove gho swap dash

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
     >
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <h1>My Dashboards</h1>
-        <GhoSwapDash />
+        {/* <GhoSwapDash /> */}
         <AaveDash />
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
